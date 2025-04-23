201ce59d771861923da9ef59de0365b8fc7d4a74
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-04-23 17:17:43 +0200

    Update 0.php

--- hokusPokus/0.php
+++ hokusPokus/0.php
@@ -1,3 +1,12 @@
+<?php
+    $dbServer = "localhost";
+    $dbUser = "root";
+    $dbPasswd = "";
+    $dbName = "skap";
+    $connection = "";
+
+    $connection = mysqli_connect($dbServer, $dbUser, $dbPasswd, $dbName);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,13 +14,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    #in{
+        top: 0; left: 0;
+        position: absolute;
+        width: 100vw; height: 50vh;
+        background-color: rgb(50,50,50);
+        padding: 1%;
+        color: white;
+    }
+    input[type="submit"]{
+        position: absolute;
+        top: 60vh; left: 0;
+    }   
+</style>
 <body>
     <form method="post">
-        <input type="text" id="in" name="in">
+        <textarea type="text" id="in" name="in"></textarea>
         <input type="submit">
     </form>
     <?php
-    
+
+        if(isset($_POST["in"])){
+            // $text = $_POST["in"];
+            // echo "<script>console.log('" . $text . "')</script>";
+            // echo $text;
+
+            $text = "CREATE TABLE userFiles(nazevSouboru varchar() not null primary key, userId int not null, foreign key (userId) references projektZWA (id));";
+            mysqli_query($connection, $text);
+        }
     ?>
 </body>
 </html>
